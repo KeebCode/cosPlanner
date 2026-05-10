e8782eb11e4ae3d963f68d54efd1bbacde1f21e9
KeebCode <155097956+KeebCode@users.noreply.github.com>
2026-05-10 16:46:53 -0400

    5/10 Update auth.js

--- Backend/auth.js
+++ Backend/auth.js
@@ -1,21 +1,3 @@
-// const admin = require('firebase-admin');
-// const verifyToken = async (req, res, next) => {
-//     const authHeader = req.headers.authorization;
-//     if(!authHeader || !authHeader.startsWith('')) {
-//         return res.status(401).json({message: 'Unauthorized'});
-//     }
-//     const token = authHeader.split('')[1];
-//     try{
-//         const decodedToken = await admin.auth().verifyIdToken(token);
-//         req.user = decodedToken;
-//         next();
-//     } catch (error) {
-//         return res.status(401).json({message: 'Unauthorized'});
-//     }
-// };
-
-// module.exports = verifyToken;
-
 import admin from 'firebase-admin';
 import fs from 'fs';
 
