b7775136f29d00ac0ee9ecbd3761d997b709874b
KeebCode <155097956+KeebCode@users.noreply.github.com>
2026-05-18 00:25:44 -0400

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,46 @@
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
-import Layout from "./components/layout/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Layout from "./components/layout/Layout";
+import RequireAuth from "./components/auth/RequireAuth";
+import { AuthProvider } from "./context/AuthContext";
+
+import LoginPage from "./pages/LoginPage";
 import ProjectsPage from "./pages/ProjectsPage";
-import GarmentsPlanningPage from "./pages/GarmentPlanningPage";
+import GarmentPlanningPage from "./pages/GarmentPlanningPage";
+import ProjectSetupPage from "./pages/ProjectSetupPage";
 import InventoryPage from "./pages/InventoryPage";
-import ChecklistPage from "./pages/ChecklistPage";
+import ChecklistPage from "./pages/ChecklistPage"; // new
+import ChecklistDashboard from "./pages/ChecklistDashboard";
+import ProfilePage from "./pages/ProfilePage";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path = "/" element = {<Layout />}>
-                    <Route index element = {<Navigate to ="/projects" replace />}/>
-                    <Route path = "/projects" element = {<ProjectsPage />} />
-                    <Route path = "/garment" element = {< GarmentsPlanningPage/>} />
-                    <Route path = "/inventory" element = {< InventoryPage/>} />
-                    <Route path = "/checklist" element = {< ChecklistPage/>} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+
+          <Route
+            path="/"
+            element={
+              <RequireAuth>
+                <Layout />
+              </RequireAuth>
+            }
+          >
+            <Route index element={<ProjectsPage />} />
+            <Route path="checklist" element={<ChecklistDashboard />} />
+            <Route path="project/:id/setup" element={<ProjectSetupPage />} />
+            <Route
+              path="project/:id/planning"
+              element={<GarmentPlanningPage />}
+            />
+            <Route path="project/:id/inventory" element={<InventoryPage />} />
+            <Route path="project/:id/checklist" element={<ChecklistPage />} />
+            <Route path="profile" element={<ProfilePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
